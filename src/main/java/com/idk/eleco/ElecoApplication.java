d8deb878289d014212731400798546053cc6da22
EricZhao666 <1339278927@qq.com>
2022-12-05 20:48:59 +0800

    first commit

---
+++ src/main/java/com/idk/eleco/ElecoApplication.java
@@ -0,0 +1,13 @@
+package com.idk.eleco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idk/eleco/ElecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idk.eleco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
