1be4c7bd4509d91d7912c7f4bfb878bf99167e83
KAIFENG-ch <85030219+KAIFENG-ch@users.noreply.github.com>
2022-12-20 18:25:45 +0800

    发送评论

---
+++ src/main/java/com/idk/eleco/controller/CommentController.java
@@ -0,0 +1,35 @@
+package com.idk.eleco.controller;
+
+import com.idk.eleco.serivce.CommentService;
+import com.idk.eleco.util.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/comment")
+@Api("评论功能")
+public class CommentController {
+
+    @Resource
+    CommentService commentService;
+
+    @ApiOperation("添加评论")
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseResult<?> comment(@RequestParam("postId") String postId,
+                                     @RequestParam("userId") String userId,
+                                     @RequestParam("postComment") String postComment,
+                                     @RequestParam("commentFlour") int commentFlour,
+                                     @RequestParam(value = "quoteFlour", defaultValue = "0") int quoteFlour,
+                                     @RequestParam(value = "quoteCommentId", defaultValue = "") String quoteCommentId) {
+        return commentService.comment(postId, userId, postComment, commentFlour, quoteFlour, quoteCommentId);
+    }
+
+}
---
+++ src/main/java/com/idk/eleco/serivce/CommentService.java
@@ -0,0 +1,68 @@
+package com.idk.eleco.serivce;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.idk.eleco.mapper.CommentMapper;
+import com.idk.eleco.mapper.UserMapper;
+import com.idk.eleco.model.entity.Comment;
+import com.idk.eleco.model.entity.User;
+import com.idk.eleco.util.ResponseResult;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Service
+@Repository
+public class CommentService {
+
+    @Resource
+    CommentMapper commentMapper;
+
+    @Resource
+    UserMapper userMapper;
+
+    public ResponseResult<?> comment(String postId, String userId, String postComment,
+                                     int commentFlour, int quoteFlour, String quoteCommentId) {
+        if (ObjectUtils.isEmpty(quoteCommentId)) {
+            // 无父评论
+            QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+            userQueryWrapper.eq("user_id", userId);
+            String username = userMapper.selectOne(userQueryWrapper).getUserName();
+            Comment comment = Comment.builder()
+                    .commentPostId(postId)
+                    .commentUserId(userId)
+                    .commentUserName(username)
+                    .postComment(postComment)
+                    .commentFlour(commentFlour)
+                    .isQuote(false)
+                    .commentTime(new Date())
+                    .build();
+            commentMapper.insert(comment);
+            return new ResponseResult<>(200, "发表成功");
+        } else {
+            // 有父评论
+            QueryWrapper<Comment> commentQueryWrapper = new QueryWrapper<>();
+            commentQueryWrapper.eq("commentId", quoteCommentId);
+            Comment quoteComment = commentMapper.selectOne(commentQueryWrapper);
+            QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+            userQueryWrapper.eq("user_id", userId);
+            String username = userMapper.selectOne(userQueryWrapper).getUserName();
+            Comment comment = Comment.builder()
+                    .commentPostId(postId)
+                    .commentUserId(userId)
+                    .commentUserName(username)
+                    .postComment(postComment)
+                    .isQuote(true)
+                    .quoteFlour(quoteFlour)
+                    .quoteUserName(quoteComment.getCommentUserName())
+                    .quoteUserId(quoteComment.getCommentUserId())
+                    .quoteComment(quoteComment.getPostComment())
+                    .commentTime(new Date())
+                    .build();
+            commentMapper.insert(comment);
+            return new ResponseResult<>(200, "发表成功");
+        }
+    }
+}
